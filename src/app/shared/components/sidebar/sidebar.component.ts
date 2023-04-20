bd80de9db45443570803884d7693bd9b4cfe3799
Juan Jesús Martin <juanjesus.martin@viewnext.com>
2023-04-20 14:58:37 +0200

    fin de la seccion 7 del curso

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Gif } from 'src/app/gifs/interfaces/gifs.interfaces';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+  constructor(private gifsServices: GifsService) { }
+
+  get tags(): string[] {
+    return this.gifsServices.tagsHistory;
+  }
+
+  public searchTag(tag: string): void {
+    this.gifsServices.searchTag(tag);
+  }
+}
